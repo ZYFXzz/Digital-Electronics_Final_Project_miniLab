588aefee953c59f801c24afbca98d698707c3aa4
Michael <yx2568@nyu.edu>
2023-04-01 23:18:08 -0400

    single keyboard tested

--- Final_Project_miniLab.ino
+++ Final_Project_miniLab.ino
@@ -1,11 +1,18 @@
-
+int buttonPin = 32;
 
 void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
   // put your setup code here, to run once:
+}
 
+void checkMechnicalSwitch() {
+  if (digitalRead(buttonPin) == LOW) {
+    Serial.println("buttonPressed");
+  } else if (digitalRead(buttonPin) == HIGH) {
+  }
 }
 
 void loop() {
+  checkMechnicalSwitch();
   // put your main code here, to run repeatedly:
-
 }
